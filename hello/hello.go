2450c61f222f354161d1a1f4d8385cc5a3db0e4f
abdel saymeh <a.saymeh@proton.me>
2024-10-10 17:07:55 -0400

    add different languages

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	switch language {
+	case spanish:
+		return spanishHelloPrefix + name
+	case french:
+		return frenchHelloPrefix + name
+	default:
+		return englishHelloPrefix + name
+	}
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
