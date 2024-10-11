d8b88d56e23f6a47db8654e89972787518f354bd
abdel saymeh <a.saymeh@proton.me>
2024-10-11 14:37:22 -0400

    add methods, and circle

--- src/ch4/shapes_test.go
+++ src/ch4/shapes_test.go
@@ -3,21 +3,33 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkPerimeter(t, &rectangle, 40.0)
+	})
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, &rectangle, 72.0)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, &circle, 314.1592653589793)
+	})
 }
