39cb0a5439494a04dcd23cff5f4b257b67de9b80
abdel saymeh <a.saymeh@proton.me>
2024-10-11 00:48:09 -0400

    control how many times repeated, add example and benchmark

--- src/ch2/iteration/repeat.go
+++ src/ch2/iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(char string) string {
+func Repeat(char string, amount int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < amount; i++ {
 		repeated += char
 	}
 	return repeated
