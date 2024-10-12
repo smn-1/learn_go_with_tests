f64da113e44ba1305acccade13de67272e66d049
abdel saymeh <a.saymeh@proton.me>
2024-10-11 20:24:56 -0400

    make test use struct, and change area test

--- src/ch4/shapes.go
+++ src/ch4/shapes.go
@@ -4,7 +4,6 @@ import "math"
 
 type Shape interface {
 	Area() float64
-	Perimeter() float64
 }
 
 type Rectangle struct {
@@ -30,4 +29,13 @@ func (c Circle) Area() float64 {
 
 func (c Circle) Perimeter() float64 {
 	return 2 * math.Pi * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * 0.5
 }
--- src/ch4/shapes_test.go
+++ src/ch4/shapes_test.go
@@ -3,11 +3,11 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+	checkPerimeter := func(t testing.TB, r *Rectangle, want float64) {
 		t.Helper()
-		got := shape.Perimeter()
+		got := r.Perimeter()
 		if got != want {
-			t.Errorf("got %g want %g", got, want)
+			t.Errorf("got %g hasArea %g", got, want)
 		}
 	}
 	t.Run("rectangle", func(t *testing.T) {
@@ -17,19 +17,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12.0, 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12.0, 6.0}, hasArea: 36.0},
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, &rectangle, 72.0)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, &circle, 314.1592653589793)
-	})
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g hasArea %g", tt.shape, got, tt.hasArea)
+			}
+		})
+
+	}
 }
