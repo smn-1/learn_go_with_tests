64e480b04d7a8d1a6dc299ba9005e342e1f2f529
abdel saymeh <a.saymeh@proton.me>
2024-10-14 18:07:22 -0400

    make timeout configurable for testing purposes

--- src/ch10/racer.go
+++ src/ch10/racer.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(a, b string) (winner string) {
+var fiveSecondTimeout = 5 * time.Second
+
+func Racer(a, b string) (winner string, err error) {
+	return configurableRacer(a, b, fiveSecondTimeout)
+}
+
+func configurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
